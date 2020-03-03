207b71b1c37ec823105b7f8ccf85760236cf22d2
E~ <elle.dot2019@gmail.com>
2020-03-03 11:49:21 +0000

    File Migration and Git Init
    This is all that's been done so far. WIP!

---
+++ Assets/Scripts/timerScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerScript : MonoBehaviour {
+
+    public int minutesPassed;
+    public int secondsPassed;
+    private string minuteString;
+    private string secondString;
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+        minutesPassed = 0;
+        secondsPassed = 0;
+
+        Invoke("timerIncrement", 3);
+
+    }
+
+    public void timerIncrement() {
+
+        Invoke("timerIncrement", 1);
+
+        if (secondsPassed < 59) {
+            secondsPassed += 1;
+        } else {
+            minutesPassed += 1;
+            secondsPassed = 0;
+        }
+
+        if (minutesPassed < 10) {
+            minuteString = "0" + minutesPassed.ToString();
+        } else {
+            minuteString = minutesPassed.ToString();
+        }
+
+        if (secondsPassed < 10) {
+            secondString = "0" + secondsPassed.ToString("f0");
+        } else {
+            secondString = secondsPassed.ToString("f0");
+        }
+
+            GetComponent<Text>().text = minuteString + ":" + secondString;
+
+    }
+
+    public void playPause() {
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/viewLoad.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class viewLoad : MonoBehaviour {
+
+    public string Destination;
+    private bool fadeAway;
+    public Image FadeView;
+    public GameObject FadeViewObj;
+    
+
+    // Start is called before the first frame update
+    void Start() {
+
+        // fades the image out when view loaded
+        StartCoroutine(FadeImage(false));
+        fadeAway = false;
+
+    }
+
+    // Define new functions here
+    public void LoadView() {
+
+        FadeViewObj.SetActive(true);
+        StartCoroutine(FadeImage(true));
+        fadeAway = true;
+
+    }
+
+    public void enableDisable() {
+
+        if (fadeAway) {
+            SceneManager.LoadScene(Destination);
+        } else {
+            FadeViewObj.SetActive(false);
+        }
+
+    }
+
+    IEnumerator FadeImage(bool fadeAway) {
+
+        Invoke("enableDisable", 1);
+
+        // fade out
+        if (!fadeAway) {
+            // decreasing i
+            for (float i = 1; i >= 0; i -= Time.deltaTime) {
+                // set color with i as alpha
+                FadeView.GetComponent<Image>().color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+        }
+
+        // fade in
+        else {
+            // increasing i
+            for (float i = 0; i <= 1; i += Time.deltaTime) {
+                // set color with i as alpha
+                FadeView.GetComponent<Image>().color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+        }
+    }
+
+}
