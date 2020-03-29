76ef5f1a1abd33ccf36d92d07241fda92c2f9467
E~ <elle.dot2019@gmail.com>
2020-03-29 03:01:25 +0100

    Collider additions
    -Added collision detection for fingers and targets!
    -Improved the spawing of targets
    -Begun work on movement
    -Condensed scripts into fewer

--- Assets/Scripts/gameScript.cs
+++ Assets/Scripts/gameScript.cs
@@ -7,16 +7,18 @@ using UnityEngine.Video;
 
 public class gameScript : MonoBehaviour {
 
-    private int minutesPassed;
-    private int secondsPassed;
+    //Variables for the core game and timer mechanics
+    public int minutesPassed;
+    public int secondsPassed;
     private string minuteString;
     private string secondString;
     public bool isPaused;
     public bool gameStarted;
     private bool pauseFadeAway;
-    private int playerScore;
+    public int playerScore;
     private int displayedScore;
 
+    //GameObjects and other elements
     public Text timeLabel;
     public Text scoreLabel;
     public Text pauseTimeLabel;
@@ -26,13 +28,30 @@ public class gameScript : MonoBehaviour {
     public VideoPlayer starsAnim1;
     public VideoPlayer starsAnim2;
     public VideoPlayer countdownTimer;
-    
+    public GameObject pauseButton;
+
+    //Variables for sorting hit detection and platforms
+    public static int targetsToCalculate;
+    private GameObject[] platforms;
+    private float spawnRate;
+    private float time;
+    private float distanceFromCamera = 10f;
+    private Vector3 objectPos;
+
 
     // Start is called before the first frame update
     void Start() {
 
         minutesPassed = 0;
         secondsPassed = 0;
+
+        Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, distanceFromCamera));
+
+        //Load Platforms into Array
+        platforms = Resources.LoadAll<GameObject>("targets");
+        // Set Spawn Rate
+        spawnRate = 5.0f;
+
         Invoke("countdownStart", 1);
 
     }
@@ -57,8 +76,53 @@ public class gameScript : MonoBehaviour {
 
     void Update() {
 
-        if (displayedScore < playerScore)
+        if (!isPaused && !gameStarted)
         {
+
+            time += Time.deltaTime;
+
+            if (time > spawnRate)
+            {
+
+                print("Tried generating new platform");
+
+                //Generate Random Number and Instantiate Associated Array Item
+                int objectNum = UnityEngine.Random.Range(0, 0);
+                GameObject target = Instantiate(platforms[objectNum]);
+                //platform.AddComponent<yMovement>();
+                //Create Random Start Positon for Object and apply to Object
+
+                //Roll for above or below spawn
+                if (UnityEngine.Random.Range(0, 2) == 1) {
+                    objectPos = new Vector3((UnityEngine.Random.Range(-8, 8)), 5, 10);
+                } else {
+                    objectPos = new Vector3((UnityEngine.Random.Range(-8, 8)), -5, 10);
+                }
+
+                target.transform.position = objectPos;
+                //Reset Time Variable
+                time = 0.0f;
+            }
+
+        }
+
+        //Test to see if the target has been tapped
+        if (Input.GetMouseButtonDown(0)) {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                //Target has been tapped and can be destroyed
+                Destroy(hit.collider.gameObject);
+
+                playerScore += 100 + ((minutesPassed + 1) * (secondsPassed + (minutesPassed * 60)));
+
+            }
+
+        }
+
+        if (displayedScore < playerScore) {
             displayedScore++;
             if (displayedScore > playerScore) {
                 displayedScore = playerScore;
@@ -75,32 +139,30 @@ public class gameScript : MonoBehaviour {
 
         Invoke("timerIncrement", 1);
 
-        if (!isPaused) {
-
-            if (secondsPassed < 59) {
-                secondsPassed += 1;
-            } else {
-                minutesPassed += 1;
-                secondsPassed = 0;
-            }
-
-            if (minutesPassed < 10) {
-                minuteString = "0" + minutesPassed.ToString();
-            } else {
-                minuteString = minutesPassed.ToString();
-            }
-
-            if (secondsPassed < 10) {
-                secondString = "0" + secondsPassed.ToString("f0");
-            } else {
-                secondString = secondsPassed.ToString("f0");
-            }
-
-            timeLabel.text = minuteString + ":" + secondString;
-            pauseTimeLabel.text = minuteString + ":" + secondString;
-
-            playerScore += (minutesPassed * 20) + 20;
+        if (secondsPassed < 59) {
+            secondsPassed += 1;
+        } else {
+            minutesPassed += 1;
+            secondsPassed = 0;
         }
+
+        if (minutesPassed < 10) {
+            minuteString = "0" + minutesPassed.ToString();
+        } else {
+            minuteString = minutesPassed.ToString();
+        }
+
+        if (secondsPassed < 10) {
+            secondString = "0" + secondsPassed.ToString("f0");
+        } else {
+            secondString = secondsPassed.ToString("f0");
+        }
+
+        timeLabel.text = minuteString + ":" + secondString;
+        pauseTimeLabel.text = minuteString + ":" + secondString;
+
+        //Grant player score based on time as well
+        playerScore += (minutesPassed * 20) + 20;
 
         //scoreLabel.text = playerScore.ToString("n0") + "pts";
         //pauseScoreLabel.text = playerScore.ToString("n0") + "pts";
@@ -116,6 +178,8 @@ public class gameScript : MonoBehaviour {
             starsAnim2.Pause();
             isPaused = true;
 
+            CancelInvoke("timerIncrement");
+
             // fades the image in when pausing
             StartCoroutine(FadeImage(true));
             pauseFadeAway = true;
@@ -126,6 +190,8 @@ public class gameScript : MonoBehaviour {
             print("Game RESUMED!");
             starsAnim1.Play();
             starsAnim2.Play();
+
+            Invoke("timerIncrement", 1);
 
             // fades the image out when resuming
             StartCoroutine(FadeImage(false));
@@ -142,8 +208,7 @@ public class gameScript : MonoBehaviour {
         }
     }
 
-    IEnumerator FadeImage(bool pauseFadeAway)
-    {
+    IEnumerator FadeImage(bool pauseFadeAway) {
 
         Invoke("enableDisable", 0.4f);
 
@@ -151,7 +216,7 @@ public class gameScript : MonoBehaviour {
         if (!pauseFadeAway) {
             // decreasing i
             for (float i = 0.4f; i >= 0; i -= Time.deltaTime) {
-                // set color with i as alpha
+                // set to white, but with i as alpha
                 pauseMenu.GetComponent<Image>().color = new Color(255, 255, 255, i);
                 yield return null;
             }
@@ -161,7 +226,7 @@ public class gameScript : MonoBehaviour {
         else {
             // increasing i
             for (float i = 0; i <= 0.4f; i += Time.deltaTime) {
-                // set color with i as alpha
+                // set to white, but with i as alpha
                 pauseMenu.GetComponent<Image>().color = new Color(255, 255, 255, i);
                 yield return null;
             }
--- Assets/Scripts/randomLoader.cs
+++ Assets/Scripts/randomLoader.cs
@@ -4,14 +4,18 @@ using System.Collections.Generic;
 
 public class randomLoader : MonoBehaviour {
 
+    public static int targetsToCalculate;
     private GameObject[] platforms;
     private float spawnRate;
     private float time;
     private bool pauseCheck;
     private bool gameActive;
     private float distanceFromCamera = 10f;
+    private int minutesPassedCheck;
+    private int secondsPassedCheck;
     public gameScript isPaused;
     public gameScript gameStarted;
+    private Vector3 objectPos;
 
     // Use this for initialization
     void Start()
@@ -42,16 +46,43 @@ public class randomLoader : MonoBehaviour {
 
                 //Generate Random Number and Instantiate Associated Array Item
                 int objectNum = Random.Range(0,0);
-                GameObject platform = Instantiate(platforms[objectNum]);
+                GameObject target = Instantiate(platforms[objectNum]);
                 //platform.AddComponent<yMovement>();
                 //Create Random Start Positon for Object and apply to Object
-                Vector3 objectPos = new Vector3((Random.Range(-8, 8)), -3, 10);
-                platform.transform.position = objectPos;
+
+                //Roll for above or below spawn
+                if (Random.Range(0, 2) == 1) {
+                    objectPos = new Vector3((Random.Range(-8, 8)), 5, 10);
+                } else {
+                    objectPos = new Vector3((Random.Range(-8, 8)), -5, 10);
+                }
+                
+                target.transform.position = objectPos;
                 //Reset Time Variable
                 time = 0.0f;
             }
 
         }
-        
+
+        //Test to see if the target has been tapped
+        if (Input.GetMouseButtonDown(0)) {
+
+            //secondsPassedCheck = gameScript.secondsPassed;
+            //minutesPassedCheck = gameScript.minutesPassed;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                //Target has been tapped and can be destroyed
+                Destroy(hit.collider.gameObject);
+
+                targetsToCalculate++;
+
+            }
+
+        }
+
     }
+
 }
