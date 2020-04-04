31cbac6bb01ef16b47b0a9dd590a850aa61c93ad
E~ <elle.dot2019@gmail.com>
2020-04-04 15:59:38 +0100

    Main Menu and About Adjustments
    -Added warning before player opens the Albert URL on the main menu
    -Added nice fader to URL warning
    -Tweaked layout of the About page

--- Assets/Scripts/welcomeScript.cs
+++ Assets/Scripts/welcomeScript.cs
@@ -8,6 +8,8 @@ public class welcomeScript : MonoBehaviour {
     public GameObject welcomeView;
     public GameObject welcomeBox;
     public GameObject welcomeButton;
+    public GameObject confirmButtonObject;
+    public Button confirmButton;
     public Text welcomeTitle;
     public Text welcomeBody;
     private int isFirstLoad = 0;
@@ -15,10 +17,10 @@ public class welcomeScript : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
 
-        isFirstLoad = PlayerPrefs.GetInt("isFirstLoad", 0);
+        //isFirstLoad = PlayerPrefs.GetInt("isFirstLoad", 0);
 
-        if (isFirstLoad == 1) {
-            welcomeView.SetActive(false);
+        if (isFirstLoad == 0) {
+            welcomeView.SetActive(true);
         }
 
         PlayerPrefs.SetInt("isFirstLoad", 1);
@@ -31,21 +33,63 @@ public class welcomeScript : MonoBehaviour {
 
     }
 
-    IEnumerator FadeWelcome(bool willDisappear) {
+    public void openAlbertURL() {
+
+        welcomeView.SetActive(true);
+        StartCoroutine(FadeWelcome2(true));
+
+        confirmButtonObject.SetActive(true);
+
+        welcomeBody.alignment = TextAnchor.UpperCenter;
+        welcomeTitle.text = "Are you sure?";
+        welcomeBody.text = "This will take you to the We Are Albert Homepage.\nPlease note Climate Control is in no way affiliated with We Are Albert.\n(URL: https://wearealbert.org/)";
+        welcomeTitle.fontSize = 50;
+        welcomeBody.fontSize = 30;
+    }
+
+    public void confirmButtonPressed() {
+
+        Application.OpenURL("https://wearealbert.org//");
+        Debug.Log("Website load attempted");
+
+    }
+
+    IEnumerator FadeWelcome(bool welcomeFadeInOut) {
+
+        confirmButtonObject.SetActive(false);
 
         // fadeout for welcome box
         for (float i = 1; i >= 0; i -= Time.deltaTime)
         {
-            // set to white, but with i as alpha
+
             welcomeView.GetComponent<Image>().color = new Color(0, 0, 0, i);
-            welcomeBox.GetComponent<Image>().color = new Color(255, 255, 255, i);
-            welcomeButton.GetComponent<Image>().color = new Color(255, 255, 255, i);
+            welcomeBox.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, i);
+            welcomeButton.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, i);
+            confirmButton.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, i);
             welcomeTitle.color = new Color(0.2f, 0.2f, 0.2f, i);
             welcomeBody.color = new Color(0.2f, 0.2f, 0.2f, i);
             yield return null;
         }
 
         welcomeView.SetActive(false);
+
+    }
+
+    IEnumerator FadeWelcome2(bool welcomeFadeInOut) {
+
+        welcomeView.SetActive(true);
+
+        // fadeout for welcome box
+        for (float i = 0.0f; i <= 1.0f; i += Time.deltaTime) {
+
+            welcomeView.GetComponent<Image>().color = new Color(0, 0, 0, i);
+            welcomeBox.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, i);
+            welcomeButton.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, i);
+            confirmButton.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, i);
+            welcomeTitle.color = new Color(0.2f, 0.2f, 0.2f, i);
+            welcomeBody.color = new Color(0.2f, 0.2f, 0.2f, i);
+            yield return null;
+        }
 
     }
 
